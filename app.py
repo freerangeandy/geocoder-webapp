a4d974c41b823e006f3b444e76706669175a56c9
freerangeandy <freerangeandy@gmail.com>
2020-11-02 14:59:40 -0500

    validate csv file for having 'address' column, otherwise show error

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ def index():
 def success():
     if request.method == 'POST':
         csv_file = request.files["csv_file"]
-        content = pandas.read_csv(csv_file, index_col="ID")
-
-        return render_template("index.html", table="table goes here")
+        csv_df = pandas.read_csv(csv_file, index_col="ID")
+        if "Address" in csv_df.columns or "address" in csv_df.columns:
+            return render_template("index.html", table="table goes here")
+        else:
+            return render_template("index.html", error="Please make sure you have an address column in your CSV file!")
 
 if __name__ == '__main__':
     app.debug = True
