3670cb96e448e5b71e6fde9dadb2020f8e9d432c
freerangeandy <freerangeandy@gmail.com>
2020-11-02 15:45:15 -0500

    format latitude/longitude columns (capitalize labels, 6 decimal places)

--- geocode_script.py
+++ geocode_script.py
@@ -6,5 +6,7 @@ def geocode_addresses(df, header):
     def get_coords(row):
         return arc.geocode(row[header])[1]
 
-    df[["latitude", "longitude"]] = df.apply(get_coords, axis=1).tolist()
+    df[["Latitude", "Longitude"]] = df.apply(get_coords, axis=1).tolist()
+    df["Latitude"] = df["Latitude"].round(decimals=6)
+    df["Longitude"] = df["Longitude"].round(decimals=6)
     return df
