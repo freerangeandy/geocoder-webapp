3baeb683f03fcaac2e670309ffc705e1fe140f74
freerangeandy <freerangeandy@gmail.com>
2020-11-02 14:47:05 -0500

    read uploaded CSV into pandas dataframe

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pandas
 
 app = Flask(__name__)
 
@@ -9,8 +10,11 @@ def index():
 @app.route("/success", methods=['POST'])
 def success():
     if request.method == 'POST':
+        csv_file = request.files["csv_file"]
+        content = pandas.read_csv(csv_file, index_col="ID")
+
         return render_template("index.html", table="table goes here")
-        
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
