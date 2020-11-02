f8e81c12e5b3035fc6fc4d13334e96b0f7aa6b10
freerangeandy <freerangeandy@gmail.com>
2020-11-02 15:34:37 -0500

    wrap geocoding inside a function (for export)

--- geocode_script.py
+++ geocode_script.py
@@ -1,13 +1,10 @@
-import os
 import pandas
 from geopy.geocoders import ArcGIS
 
-arc = ArcGIS()
+def geocode_addresses(df, header):
+    arc = ArcGIS()
+    def get_coords(row):
+        return arc.geocode(row[header])[1]
 
-def get_coords(row):
-    return arc.geocode(row["Address"])[1]
-
-df = pandas.read_csv("addresses.csv")
-
-df[["latitude", "longitude"]] = df.apply(get_coords, axis=1).tolist()
-print(df)
+    df[["latitude", "longitude"]] = df.apply(get_coords, axis=1).tolist()
+    return df
